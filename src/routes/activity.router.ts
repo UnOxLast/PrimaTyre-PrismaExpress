eb00e88d8ad7471ee1e8a67d2573eeed8dc25327
UnOxLast <diqy2002@gmail.com>
2025-07-17 14:17:18 +0700

    minor fix

--- src/routes/activity.router.ts
+++ src/routes/activity.router.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { createActivityTyre, exportActivityToExcel, getActivityByTyreId, getAllActivityTyre } from "../controllers/activity.controller";
+import { protect } from "../middlewares/auth.middleware";
 
 const activityRouter = Router();
 
-activityRouter.get("/", getAllActivityTyre)
-// activityRouter.get("/tyre/:id", getActivityByTyreId)
-activityRouter.get("/:id", getActivityByTyreId)
-activityRouter.post("/", createActivityTyre)
-// activityRouter.get("/export", exportActivityToExcel)
-// activityRouter.put("/:id", updateActivity)
+activityRouter.get("/", protect, getAllActivityTyre)
+// activityRouter.get("/tyre/:id", protect, getActivityByTyreId)
+activityRouter.get("/:id", protect, getActivityByTyreId)
+activityRouter.post("/", protect, createActivityTyre)
+activityRouter.get("/export", protect, exportActivityToExcel)
+// activityRouter.put("/:id", protect, updateActivity)
 
 export default module.exports = activityRouter
